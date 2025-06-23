3efdc0096769f647b2ac0cdb4b898ca4741db325
Noam Alum <noam.alum@gmail.com>
2025-06-23 19:56:32 +0300

    Removed old files to add them as submodules

--- chronicle_web_ui/components/ui/card.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-
-export function Card({ children, className = "" }) {
-  return <div className={`rounded-xl border bg-white p-4 shadow ${className}`}>{children}</div>
-}
-
-export function CardContent({ children, className = "" }) {
-  return <div className={className}>{children}</div>
-}
--- chronicle_web_ui/components/ui/textarea.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react"
-
-export function Textarea({ className = "", ...props }) {
-  return <textarea className={`border rounded p-2 w-full ${className}`} {...props} />
-}
